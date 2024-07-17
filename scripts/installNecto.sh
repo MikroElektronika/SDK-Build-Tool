b2666eb1932470cbf3a8787bad1f9dcbe8e19eef
Ivan Ruzavin <ivan.ruzavin@mikroe.com>
2024-07-17 08:29:05 +0200

    Added mikroe_utils_common package

--- scripts/installNecto.sh
+++ scripts/installNecto.sh
@@ -18,7 +18,7 @@ echo "Step 2: Extract installer"
 7za x installer.7z
 
 echo "Step 3: Install NECTO"
-./installer_tmp installer --install-packages necto_installer necto_application clocks schemas database /home/runner/work/SDK-Build-Tool/MikroElektronika /home/runner/work/SDK-Build-Tool/.MIKROE/NECTOStudio7
+./installer_tmp installer --install-packages necto_installer necto_application clocks schemas database mikroe_utils_common /home/runner/work/SDK-Build-Tool/MikroElektronika /home/runner/work/SDK-Build-Tool/.MIKROE/NECTOStudio7
 
 echo "Step 4: Move installer to MIKROE if it's generated in root"
 [ -f "/home/runner/work/SDK-Build-Tool/MikroElektronika/installer_tmp" ] && mv "/home/runner/work/SDK-Build-Tool/MikroElektronika/installer_tmp" /home/runner/work/SDK-Build-Tool/MikroElektronika/installer
