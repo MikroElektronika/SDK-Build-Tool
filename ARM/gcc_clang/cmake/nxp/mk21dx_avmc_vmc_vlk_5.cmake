4ddcb396cca40add774edc0dd6c61b889d67e598
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-03-25 11:03:12 +0100

    Updated packages again

--- ARM/gcc_clang/cmake/nxp/mk21dx_avmc_vmc_vlk_5.cmake
+++ ARM/gcc_clang/cmake/nxp/mk21dx_avmc_vmc_vlk_5.cmake
@@ -1,4 +1,4 @@
-if(${MCU_NAME} MATCHES "^MKL03Z8VFG4$|^MKL03Z16VFG4$|^MKL03Z32VFG4$|^MKL03Z32CAF4$|^MKL03Z32CBF4$|^MKL03Z8VFK4$|^MKL03Z16VFK4$|^MKL03Z32VFK4$")
+if(${MCU_NAME} MATCHES "^MK21DX128AVMC5$|^MK21DX128VLK5$|^MK21DX128VMC5$|^MK21DX256AVMC5$|^MK21DX256VLK5$|^MK21DX256VMC5$")
     set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
     set(${startupFile} startup/${vendor}/${mcu_match}.s PARENT_SCOPE)
     list(APPEND local_list_include system/src/nxp/mk21dx_avmc_vmc_vlk_5/init_clock.c)
