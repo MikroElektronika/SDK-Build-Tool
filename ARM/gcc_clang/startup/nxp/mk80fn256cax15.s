0479c73bc0352ab2599d7be26ce314161ace8c47
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-04-10 11:55:25 +0200

    Fixed startup files

--- ARM/gcc_clang/startup/nxp/mk80fn256cax15.s
+++ ARM/gcc_clang/startup/nxp/mk80fn256cax15.s
@@ -317,26 +317,12 @@ Reset_Handler:
 #ifdef __STARTUP_INITIALIZE_NONCACHEDATA
     ldr    r2, =__noncachedata_start__
     ldr    r3, =__noncachedata_init_end__
-#ifdef __PERFORMANCE_IMPLEMENTATION
-/* Here are two copies of loop implementations. First one favors performance
- * and the second one favors code size. Default uses the second one.
- * Define macro "__PERFORMANCE_IMPLEMENTATION" in project to use the first one */
-    subs    r3, r2
-    ble    .LC3
-.LC2:
-    subs    r3, #4
-    ldr    r0, [r1, r3]
-    str    r0, [r2, r3]
-    bgt    .LC2
-.LC3:
-#else  /* code size implemenation */
 .LC2:
     cmp     r2, r3
     ittt    lt
     ldrlt   r0, [r1], #4
     strlt   r0, [r2], #4
     blt    .LC2
-#endif
 /* zero inited ncache section initialization */
     ldr r3, =__noncachedata_end__
     movs    r0,0
