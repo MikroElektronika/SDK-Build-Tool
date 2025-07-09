2843f8552d51e6ecc95ddae08499af688b0f6a9a
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:59:02 +0200

    Update from folder 'M054 MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/m054.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^M054LDE$|^M054LDN$|^M054ZDE$|^M054ZDN$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/m054/system_m054.c)
+    list(APPEND local_dir_install system/src/nuvoton/m054/thirdparty/m054)
+    set(${thirdpartyInstall} m054/thirdparty/m054 PARENT_SCOPE)
+endif()
