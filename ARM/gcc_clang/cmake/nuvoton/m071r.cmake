7cc75519e8620274cb49c4b7bd136d6eed07322e
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:58:36 +0200

    Update from folder 'M071R MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/m071r.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^M071R1E3AE$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/m071r/system_m071r.c)
+    list(APPEND local_dir_install system/src/nuvoton/m071r/thirdparty/m071r)
+    set(${thirdpartyInstall} m071r/thirdparty/m071r PARENT_SCOPE)
+endif()
---
+++ changelog/new_hw.md
@@ -0,0 +1,22 @@
+<p align="center">
+  <img src="http://www.mikroe.com/img/designs/beta/logo_small.png?raw=true" alt="MikroElektronika"/>
+</p>
+
+---
+
+# `DATE`
+
+## Changes
+
+- [`DATE`](#date)
+  - [Changes](#changes)
+    - [NEW HARDWARE](#new-hardware)
+
+### NEW HARDWARE
+
+Support added for following hardware:
+
++ M071R MCU Support package for GCC & Clang
+  + M071R1E3AE
+
+---
