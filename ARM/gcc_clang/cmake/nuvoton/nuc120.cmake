ca7e8a17234f2187273d797a022660bc0c84fcfd
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:58:22 +0200

    Update from folder 'NUC120 MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/nuc120.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^NUC120LC1DN$|^NUC120LD2DN$|^NUC120LE3DN$|^NUC120RC1DN$|^NUC120RD2DN$|^NUC120RE3DN$|^NUC120VE3DN$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/nuc120/system_nuc120.c)
+    list(APPEND local_dir_install system/src/nuvoton/nuc120/thirdparty/nuc120)
+    set(${thirdpartyInstall} nuc120/thirdparty/nuc120 PARENT_SCOPE)
+endif()
---
+++ changelog/new_hw.md
@@ -0,0 +1,28 @@
+<p align="center">
+  <img src="http://www.mikroe.com/img/designs/beta/logo_small.png?raw=true" alt="MikroElektronika"/>
+</p>
+
+---
+
+# `DATE`
+
+## Changes
+
+- [`DATE`](#date)
+  - [Changes](#changes)
+    - [NEW HARDWARE](#new-hardware)
+
+### NEW HARDWARE
+
+Support added for following hardware:
+
++ NUC120 MCU Support package for GCC & Clang
+  + NUC120LC1DN
+  + NUC120LD2DN
+  + NUC120LE3DN
+  + NUC120RC1DN
+  + NUC120RD2DN
+  + NUC120RE3DN
+  + NUC120VE3DN
+
+---
