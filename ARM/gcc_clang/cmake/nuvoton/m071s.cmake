d756af15a04233463c4f2db1534e19c98d7166b4
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:58:39 +0200

    Update from folder 'M071S MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/m071s.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^M071SE3AE$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/m071s/system_m071s.c)
+    list(APPEND local_dir_install system/src/nuvoton/m071s/thirdparty/m071s)
+    set(${thirdpartyInstall} m071s/thirdparty/m071s PARENT_SCOPE)
+endif()
