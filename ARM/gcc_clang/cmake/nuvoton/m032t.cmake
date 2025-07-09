b8a0841e5b580129d741d685106cc173c9d7e8e8
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:57:21 +0200

    Update from folder 'M032T MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/m032t.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^M032TC1AE$|^M032TD2AE$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/m032t/system_m032t.c)
+    list(APPEND local_dir_install system/src/nuvoton/m032t/thirdparty/m032t)
+    set(${thirdpartyInstall} m032t/thirdparty/m032t PARENT_SCOPE)
+endif()
