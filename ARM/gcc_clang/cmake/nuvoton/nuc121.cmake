883309fd549f8474f758ec8a0fee34bfa63d05e6
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:57:47 +0200

    Update from folder 'NUC121 MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/nuc121.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^NUC121LC2AE$|^NUC121SC2AE$|^NUC121ZC2AE$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/nuc121/system_nuc121.c)
+    list(APPEND local_dir_install system/src/nuvoton/nuc121/thirdparty/nuc121)
+    set(${thirdpartyInstall} nuc121/thirdparty/nuc121 PARENT_SCOPE)
+endif()
---
+++ changelog/new_hw.md
@@ -0,0 +1,24 @@
+<p align="center">
+  <img src="http://www.mikroe.com/img/designs/beta/logo_small.png?raw=true" alt="MikroElektronika"/>
+</p>
+
+---
+
+# `DATE`
+
+## Changes
+
+- [`DATE`](#date)
+  - [Changes](#changes)
+    - [NEW HARDWARE](#new-hardware)
+
+### NEW HARDWARE
+
+Support added for following hardware:
+
++ NUC121 MCU Support package for GCC & Clang
+  + NUC121LC2AE
+  + NUC121SC2AE
+  + NUC121ZC2AE
+
+---
