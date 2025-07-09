e8a93a96956aeb73a8310974cfa28ce054b5d7d4
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:56:11 +0200

    Update from folder 'M0518 MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/m0518.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^M0518LC2AE$|^M0518LD2AE$|^M0518SC2AE$|^M0518SD2AE$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/m0518/system_m0518.c)
+    list(APPEND local_dir_install system/src/nuvoton/m0518/thirdparty/m0518)
+    set(${thirdpartyInstall} m0518/thirdparty/m0518 PARENT_SCOPE)
+endif()
---
+++ changelog/new_hw.md
@@ -0,0 +1,25 @@
+<p align="center">
+  <img src="http://www.mikroe.com/img/designs/beta/logo_small.png?raw=true" alt="MikroElektronika"/>
+</p>
+
+---
+
+# `DATE`
+
+## Changes
+
+- [`DATE`](#date)
+  - [Changes](#changes)
+    - [NEW HARDWARE](#new-hardware)
+
+### NEW HARDWARE
+
+Support added for following hardware:
+
++ M0518 MCU Support package for GCC & Clang
+  + M0518LC2AE
+  + M0518LD2AE
+  + M0518SC2AE
+  + M0518SD2AE
+
+---
