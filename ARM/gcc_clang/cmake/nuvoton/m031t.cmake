bb78784193e6b625867ce254ab5447b43cea7f9f
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:59:11 +0200

    Update from folder 'M031T MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/m031t.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^M031TB0AE$|^M031TC1AE$|^M031TD2AE$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/m031t/system_m031t.c)
+    list(APPEND local_dir_install system/src/nuvoton/m031t/thirdparty/m031t)
+    set(${thirdpartyInstall} m031t/thirdparty/m031t PARENT_SCOPE)
+endif()
---
+++ changelog/new_hw.md
@@ -0,0 +1,24 @@
+<p align="center">
+  <img src="http://www.mikroe.com/img/designs/beta/logo_small.png?raw=true" alt="MikroElektronika"/>
+</p>
+
+---
+
+# `DATE`
+
+## Changes
+
+- [`DATE`](#date)
+  - [Changes](#changes)
+    - [NEW HARDWARE](#new-hardware)
+
+### NEW HARDWARE
+
+Support added for following hardware:
+
++ M031T MCU Support package for GCC & Clang
+  + M031TB0AE
+  + M031TC1AE
+  + M031TD2AE
+
+---
