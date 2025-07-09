8559e4f92ac81c3c77b1708fd69127938b3116f4
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:58:13 +0200

    Update from folder 'NUC131 MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/nuc131.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^NUC131LC2AE$|^NUC131LD2AE$|^NUC131SC2AE$|^NUC131SD2AE$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/nuc131/system_nuc131.c)
+    list(APPEND local_dir_install system/src/nuvoton/nuc131/thirdparty/nuc131)
+    set(${thirdpartyInstall} nuc131/thirdparty/nuc131 PARENT_SCOPE)
+endif()
