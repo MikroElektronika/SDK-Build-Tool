42ee69f345b4dd7f3d8157d7cd5f706cba39e790
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:56:43 +0200

    Update from folder 'M483 MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/m483.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^M483KGCAE$|^M483KGCAE2A$|^M483KIDAE$|^M483SE8AE$|^M483SGCAE$|^M483SGCAE2A$|^M483SIDAE$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/m483/system_m483.c)
+    list(APPEND local_dir_install system/src/nuvoton/m483/thirdparty/m483)
+    set(${thirdpartyInstall} m483/thirdparty/m483 PARENT_SCOPE)
+endif()
---
+++ changelog/new_hw.md
@@ -0,0 +1,28 @@
+<p align="center">
+  <img src="http://www.mikroe.com/img/designs/beta/logo_small.png?raw=true" alt="MikroElektronika"/>
+</p>
+
+---
+
+# `DATE`
+
+## Changes
+
+- [`DATE`](#date)
+  - [Changes](#changes)
+    - [NEW HARDWARE](#new-hardware)
+
+### NEW HARDWARE
+
+Support added for following hardware:
+
++ M483 MCU Support package for GCC & Clang
+  + M483KGCAE
+  + M483KGCAE2A
+  + M483KIDAE
+  + M483SE8AE
+  + M483SGCAE
+  + M483SGCAE2A
+  + M483SIDAE
+
+---
