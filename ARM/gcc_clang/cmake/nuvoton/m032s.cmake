34575f9b2842db501d9acb6a54f3f37ac8a8122f
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:59:08 +0200

    Update from folder 'M032S MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/m032s.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^M032SE3AE$|^M032SG6AE$|^M032SG8AE$|^M032SIAAE$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/m032s/system_m032s.c)
+    list(APPEND local_dir_install system/src/nuvoton/m032s/thirdparty/m032s)
+    set(${thirdpartyInstall} m032s/thirdparty/m032s PARENT_SCOPE)
+endif()
---
+++ changelog/new_hw.md
@@ -0,0 +1,25 @@
+<p align="center">
+  <img src="http://www.mikroe.com/img/designs/beta/logo_small.png?raw=true" alt="MikroElektronika"/>
+</p>
+
+---
+
+# `DATE`
+
+## Changes
+
+- [`DATE`](#date)
+  - [Changes](#changes)
+    - [NEW HARDWARE](#new-hardware)
+
+### NEW HARDWARE
+
+Support added for following hardware:
+
++ M032S MCU Support package for GCC & Clang
+  + M032SE3AE
+  + M032SG6AE
+  + M032SG8AE
+  + M032SIAAE
+
+---
