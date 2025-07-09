cc1b5a594d01ce8e4b34005d7c01332afd9b4dda
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:58:48 +0200

    Update from folder 'MINI52 MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/mini52.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^MINI52FDE$|^MINI52LDE$|^MINI52TDE$|^MINI52ZDE$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/mini52/system_mini52.c)
+    list(APPEND local_dir_install system/src/nuvoton/mini52/thirdparty/mini52)
+    set(${thirdpartyInstall} mini52/thirdparty/mini52 PARENT_SCOPE)
+endif()
---
+++ changelog/new_hw.md
@@ -0,0 +1,25 @@
+<p align="center">
+  <img src="http://www.mikroe.com/img/designs/beta/logo_small.png?raw=true" alt="MikroElektronika"/>
+</p>
+
+---
+
+# `DATE`
+
+## Changes
+
+- [`DATE`](#date)
+  - [Changes](#changes)
+    - [NEW HARDWARE](#new-hardware)
+
+### NEW HARDWARE
+
+Support added for following hardware:
+
++ MINI52 MCU Support package for GCC & Clang
+  + MINI52FDE
+  + MINI52LDE
+  + MINI52TDE
+  + MINI52ZDE
+
+---
