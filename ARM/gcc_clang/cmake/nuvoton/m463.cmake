2c3d9b6423e38ed7a7f265da81347bc175441bad
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-07-09 09:56:23 +0200

    Update from folder 'M463 MCU Support package for GCC & Clang'

---
+++ ARM/gcc_clang/cmake/nuvoton/m463.cmake
@@ -0,0 +1,7 @@
+if(${MCU_NAME} MATCHES "^M463KGCAE$|^M463LGCAE$|^M463SGCAE$|^M463YGCAE$")
+    set(${linkerScript} linker_scripts/${vendor}/${mcu_match}.ld PARENT_SCOPE)
+    set(${startupFile} startup/${vendor}/${mcu_match}.S PARENT_SCOPE)
+    list(APPEND local_list_include system/src/nuvoton/m463/system_m463.c)
+    list(APPEND local_dir_install system/src/nuvoton/m463/thirdparty/m463)
+    set(${thirdpartyInstall} m463/thirdparty/m463 PARENT_SCOPE)
+endif()
---
+++ changelog/new_hw.md
@@ -0,0 +1,25 @@
+<p align="center">
+  <img src="http://www.mikroe.com/img/designs/beta/logo_small.png?raw=true" alt="MikroElektronika"/>
+</p>
+
+---
+
+# `DATE`
+
+## Changes
+
+- [`DATE`](#date)
+  - [Changes](#changes)
+    - [NEW HARDWARE](#new-hardware)
+
+### NEW HARDWARE
+
+Support added for following hardware:
+
++ M463 MCU Support package for GCC & Clang
+  + M463KGCAE
+  + M463LGCAE
+  + M463SGCAE
+  + M463YGCAE
+
+---
