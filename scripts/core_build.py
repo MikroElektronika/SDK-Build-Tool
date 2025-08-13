af75deab6a7baf35e33e9bdcc63a6fe72f77443e
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-08-13 15:08:44 +0200

    Added XMC for build tool

--- scripts/core_build.py
+++ scripts/core_build.py
@@ -161,7 +161,7 @@ def run_builds(changes_dict):
 # Returns the list of compilers based on the given name and type.
 def get_compilers(name, is_mcu=True):
     if is_mcu:
-        if any(substring in name for substring in ["SAM", "STM", "TM4C", "MK", "MC", "R7FA", "K32L", "KW45", "M0", "M2", "M4", "NUC", "NANO", "MINI", "M23"]):
+        if any(substring in name for substring in ["SAM", "STM", "TM4C", "MK", "MC", "R7FA", "K32L", "KW45", "M0", "M2", "M4", "NUC", "NANO", "MINI", "M23", "XMC"]):
             return compiler_list["ARM"]
         elif any(substring in name for substring in ["GD32", "RISC"]):
             return compiler_list["RISCV"]
