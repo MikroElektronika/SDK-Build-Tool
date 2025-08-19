62050e13bb5b180fe3fb342bb685fe8c5e40d122
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-08-19 11:44:35 +0200

    Let's try installing NECTO #4

--- scripts/install_necto.py
+++ scripts/install_necto.py
@@ -19,9 +19,9 @@ elif sys.platform.startswith('linux'):
     installer_arcive = 'NECTOInstaller.zip'
 elif sys.platform.startswith('darwin'):
     installer_handler = {
-        'root_path': '/home/runner',
-        'necto_path': '/home/runner/MikroElektronika',
-        'necto_path_app_data': '/home/runner/.MIKROE/NECTOStudio7',
+        'root_path': '/Users/runner',
+        'necto_path': '/Users/runner/MikroElektronika',
+        'necto_path_app_data': '/Users/runner/.MIKROE/NECTOStudio7',
         'necto_link': 'https://software-update.mikroe.com/NECTOStudio7/live/necto/macos/NECTOInstaller.dmg'
     }
     installer_arcive = 'NECTOInstaller.dmg'
@@ -48,10 +48,11 @@ def main():
     elif sys.platform.startswith('linux'):
         run_command(f"7za x {installer_zip} -o{installer_handler['root_path']}")
     elif sys.platform.startswith('darwin'):
+        # Mount the DMG
         print(f"Mounting {installer_zip}...")
-        run_command(f"hdiutil attach {installer_zip} -mountpoint {installer_handler['root_path']} -nobrowse -quiet")
+        run_command(f"hdiutil attach {installer_zip} -mountpoint /Volumes/NECTOInstaller -nobrowse -quiet")
         # Copy app bundle or installer from the DMG
-        run_command(f"cp -R {installer_handler['root_path']}/* {installer_handler['root_path']}")
+        run_command(f"cp -R /Volumes/NECTOInstaller/* {installer_zip}")
 
     print("Step 3: Install NECTO")
     run_command(f'{installer_runner} installer --install-packages necto_installer necto_application database clocks schemas mikroe_utils_common preinit unit_test_lib mikrosdk {installer_handler['necto_path']} {installer_handler['necto_path_app_data']}')
