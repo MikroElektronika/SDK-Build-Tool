83e4c406f00eeca134f1b1d1141439d7d0b5b00c
IvanRuzavin <ivan.ruzavin@mikroe.com>
2025-08-19 15:52:47 +0200

    Run after prettyfying

--- scripts/install_necto_check.py
+++ scripts/install_necto_check.py
@@ -6,69 +6,66 @@ import subprocess
 import json
 import urllib.request
 
-# -----------------------------
 # Platform-specific parameters.
-# -----------------------------
-if sys.platform.startswith("win"):
+if sys.platform.startswith('win'):
     installer = {
-        "root_path": "C:/Users/runner",
-        "necto_path": "C:/Users/runner/MikroElektronika",
-        "necto_path_app_data": "C:/Users/runner/AppData/Local/NECTOStudio7",
-        "necto_link": "https://software-update.mikroe.com/NECTOStudio7/live/necto/win/NECTOInstaller.zip",
-        "archive_name": "NECTOInstaller.zip",
-        "installer_path": "C:/Users/runner/NECTOInstaller.exe",
-        "operating_system": "Windows"
+        'root_path':            'C:/Users/runner',
+        'necto_path':           'C:/Users/runner/MikroElektronika',
+        'necto_path_app_data':  'C:/Users/runner/AppData/Local/MIKROE/NECTOStudio7',
+        'necto_link':           'https://software-update.mikroe.com/NECTOStudio7/live/necto/win/NECTOInstaller.zip',
+        'archive_name':         'NECTOInstaller.zip',
+        'installer_path':       'C:/Users/runner/NECTOInstaller.exe'
     }
-    use_zip = True
-elif sys.platform.startswith("linux"):
-    installer = {
-        "root_path": "/home/runner",
-        "necto_path": "/home/runner/MikroElektronika",
-        "necto_path_app_data": "/home/runner/.MIKROE/NECTOStudio7",
-        "necto_link": "https://software-update.mikroe.com/NECTOStudio7/live/necto/linux/NECTOInstaller.zip",
-        "archive_name": "NECTOInstaller.zip",
-        "installer_path": "/home/runner/NECTOInstaller",
-        "operating_system": "Ubutnu"
-    }
-    use_zip = True
-elif sys.platform.startswith("darwin"):
-    installer = {
-        "root_path": "/Users/runner",
-        "necto_path": "/Users/runner/MikroElektronika",
-        "necto_path_app_data": "/Users/runner/.MIKROE/NECTOStudio7",
-        "necto_link": "https://software-update.mikroe.com/NECTOStudio7/live/necto/macos/NECTOInstaller.dmg",
-        "archive_name": "NECTOInstaller.dmg",
-        "installer_path": "/Users/runner/NECTO Installer.app/Contents/MacOS/installer",
-        "operating_system": "Mac"
-    }
-    use_zip = False
 
+elif sys.platform.startswith('linux'):
+    installer = {
+        'root_path':            '/home/runner',
+        'necto_path':           '/home/runner/MikroElektronika',
+        'necto_path_app_data':  '/home/runner/.MIKROE/NECTOStudio7',
+        'necto_link':           'https://software-update.mikroe.com/NECTOStudio7/live/necto/linux/NECTOInstaller.zip',
+        'archive_name':         'NECTOInstaller.zip',
+        'installer_path':       '/home/runner/NECTOInstaller'
+    }
+
+elif sys.platform.startswith('darwin'):
+    installer = {
+        'root_path':            '/Users/runner',
+        'necto_path':           '/Users/runner/MikroElektronika',
+        'necto_path_app_data':  '/Users/runner/.MIKROE/NECTOStudio7',
+        'necto_link':           'https://software-update.mikroe.com/NECTOStudio7/live/necto/macos/NECTOInstaller.dmg',
+        'archive_name':         'NECTOInstaller.dmg',
+        'installer_path':       '/Users/runner/NECTO Installer.app/Contents/MacOS/installer'
+    }
+
+# Counter for main NECTO packages installation.
 previous_prog = 101
 
+# Function for printing only the installation progress information.
 def parse_and_print_progress(line):
     global previous_prog
-    # Ignore empty lines and Content-Length headers
-    if not line.strip() or line.startswith("Content-Length:"):
-        return
 
     try:
+        # Fetch the installation progress info from the stdout line.
+        # Example of the stdout line:
+        # {"jsonrpc": "2.0", "method": "install_progress", "params": {"status": "overall_progress", "package": "clang_format", "progress": 33}, "id": 675}Content-Length: 142
         obj = json.loads(line.split('Content-Length:')[0])
-        if obj.get("method") == "install_progress":
-            params = obj.get("params", {})
-            pkg = params.get("package")
-            prog = params.get("progress")
+        if obj.get('method') == 'install_progress':
+            params = obj.get('params', {})
+            pkg = params.get('package')
+            prog = params.get('progress')
             if pkg is not None and prog is not None and previous_prog != int(prog):
                 previous_prog = int(prog)
                 if prog != '100':
-                    print(f"[{pkg}] progress: {prog}%")
+                    print(f'[{pkg}] progress: {prog}%')
                 else:
-                    print(f"\033[32m[{pkg}] progress: {prog}%\033[32m")
+                    print(f'\033[32m[{pkg}] progress: {prog}%\033[32m')
     except json.JSONDecodeError:
-        # Not a JSON line, just print raw
+        # Not a JSON line, just print raw.
         print(line.strip())
 
+# Function for executing the commands.
 def run_command(cmd):
-    print(f"\033[36mRunning: {cmd}\033[36m")
+    print(f'\033[36mRunning: {cmd}\033[36m')
     proc = subprocess.Popen(
         cmd,
         shell=True,
@@ -76,78 +73,65 @@ def run_command(cmd):
         stderr=subprocess.STDOUT,
         text=True
     )
+
+    # Parse the line to display the installation progress.
     for line in proc.stdout:
         parse_and_print_progress(line)
     proc.wait()
+
+    # Display the error if something fails.
     if proc.returncode != 0:
-        raise RuntimeError(f"\033[31mCommand failed with exit code {proc.returncode}: {cmd}.\033[31m")
-    return proc.returncode
+        raise RuntimeError(f'\033[31mCommand failed with exit code {proc.returncode}: {cmd}.\033[31m')
 
 def main():
-    root = installer["root_path"]
-    os.makedirs(root, exist_ok=True)
+    # Make sure to create the root folder for NECTO installation.
+    os.makedirs(installer['root_path'], exist_ok=True)
 
-    # NECTO installer archive/disk image path.
-    archive_path = os.path.join(root, installer["archive_name"])
+    # Step 1 - Download NECTO installer archive/disk image.
+    print(f'Downloading NECTO Installer.')
+    archive_path = os.path.join(installer['root_path'], installer['archive_name'])
+    downloaded_location, _ = urllib.request.urlretrieve(installer['necto_link'], archive_path)
 
-    # Download NECTO installer archive/disk image.
-    print(f'\033[34mDownloading {installer['operating_system']} NECTO Installer\033[34m')
-    os.makedirs(os.path.dirname(archive_path), exist_ok=True)
-
-    # Check if it is downloaded.
-    downloaded_location, _ = urllib.request.urlretrieve(installer["necto_link"], archive_path)
+    # Step 2 - Check if NECTO installer archive/disk image is downloaded.
     if not os.path.exists(downloaded_location):
         raise FileNotFoundError(f'\033[31mDownload failed: {downloaded_location} does not exist\033[31m')
     print(f'\033[32mNECTO installer is downloaded successfully: {downloaded_location}\033[32m ({os.path.getsize(downloaded_location)} bytes)')
 
-    # Windows/Linux option.
-    if use_zip:
-        # Extract zip archive.
-        import zipfile
-        print(f"Extracting ZIP: {archive_path} -> {root}")
-        with zipfile.ZipFile(archive_path, "r") as z:
-            z.extractall(root)
-
-        # Locate runner
-        if not installer['installer_path']:
-            raise FileNotFoundError(f"\033[31mCould not locate NECTO installer binary after extracting {archive_path}\033[31m")
-
-        if sys.platform.startswith("linux"):
-            mode = os.stat(installer['installer_path']).st_mode
-            os.chmod(installer['installer_path'], mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
-
-        cmd = (
-            f"\"{installer['installer_path']}\" installer "
-            f"--install-packages "
-            f"necto_installer necto_application database clocks schemas mikroe_utils_common preinit unit_test_lib mikrosdk "
-            f"\"{installer['necto_path']}\" \"{installer['necto_path_app_data']}\""
-        )
-        run_command(cmd)
-        print("\033[32mNECTO installation completed successfully.\033[32m")
-
-    # MacOS option.
-    else:
-        # macOS DMG flow
-        mount_point = "/Volumes/NECTOInstaller"
-        print(f"Mounting DMG: {archive_path}")
-        run_command(f"hdiutil attach '{archive_path}' -mountpoint '{mount_point}' -nobrowse -quiet")
+    # Step 3 - extract runner for the installer.
+    if sys.platform.startswith('darwin'):
+        # MacOS Disk Image mounting.
+        mount_point = '/Volumes/NECTOInstaller'
+        print(f'Mounting DMG: {archive_path}')
+        run_command(f'hdiutil attach "{archive_path}" -mountpoint "{mount_point}" -nobrowse -quiet')
 
         app_path = '/Volumes/NECTOInstaller/NECTO Installer.app'
 
-        # Copy the .app out of the mounted volume first
-        dest_app = os.path.join(root, os.path.basename(app_path))
-        print(f"Copying app: '{app_path}' -> '{dest_app}'")
-        run_command(f"cp -R '{app_path}' '{dest_app}'")
+        # Copy the .app (runner) out of the mounted volume first.
+        dest_app = os.path.join(installer['root_path'], os.path.basename(app_path))
+        print(f'Copying app: "{app_path}" -> "{dest_app}"')
+        run_command(f'cp -R "{app_path}" "{dest_app}"')
 
-        cmd = (
-            f"\"{installer['installer_path']}\" installer "
-            f"--install-packages "
-            f"necto_installer necto_application database clocks schemas mikroe_utils_common preinit unit_test_lib mikrosdk "
-            f"\"{installer['necto_path']}\" \"{installer['necto_path_app_data']}\""
-        )
-        run_command(cmd)
-        print("\033[32mNECTO installation completed successfully.\033[32m")
-        return
+    else:
+        # Extract zip archive.
+        import zipfile
+        print(f'Extracting ZIP: {archive_path} -> {installer['root_path']}')
+        with zipfile.ZipFile(archive_path, 'r') as z:
+            z.extractall(installer['root_path'])
 
-if __name__ == "__main__":
+        # Make the installer file executable on linux.
+        if sys.platform.startswith('linux'):
+            mode = os.stat(installer['installer_path']).st_mode
+            os.chmod(installer['installer_path'], mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+
+    # Step 4 - Run the NECTO installer.
+    cmd = (
+        f'\'{installer['installer_path']}\' installer '
+        f'--install-packages '
+        f'necto_installer necto_application database clocks schemas mikroe_utils_common preinit unit_test_lib mikrosdk '
+        f'\'{installer['necto_path']}\' \'{installer['necto_path_app_data']}\''
+    )
+    run_command(cmd)
+    print('\033[32mNECTO installation completed successfully.\033[32m')
+
+if __name__ == '__main__':
     main()
